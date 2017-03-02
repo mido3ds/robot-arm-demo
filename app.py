1ca939d81017c950b9fb2d6261bc60d1898e954b
Mahmoud Adas <mido3ds@gmail.com>
2017-03-02 09:16:57 +0200

    add animation

--- app.py
+++ app.py
@@ -34,6 +34,13 @@ class App(tk.Frame):
         elif len(dialog) == 4:
             self.robot = Robot(l=dialog[0], a=dialog[1][0], b=dialog[2][0], theta=dialog[3])
 
+    def animate_360(self):
+        for q0 in range(0, 360):
+            for q1 in range(0, 360):
+                self.robot.q[0][0] = q0
+                self.robot.q[0][1] = q1
+                self.pen.draw()
+
 
     def make_canvas(self):
         self.canvas = draw.turtle.Canvas(self, width=800, height=800)
