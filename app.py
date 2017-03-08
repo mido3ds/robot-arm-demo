7b943feaaa2415f089b678c521443271d5db0ff0
Mahmoud Adas <mido3ds@gmail.com>
2017-03-08 10:07:36 +0200

    add this now

--- app.py
+++ app.py
@@ -17,22 +17,62 @@ class App(tk.Frame):
         tk.Frame.__init__(self, master=self.root)
         self.pack()
 
-        self.make_robot()
         self.make_canvas()
+        self.make_buttons()
+
+        
+    def make_buttons(self):
+        self.le = tk.Entry(self)
+        self.le.pack()
+        tk.Label(self, text='length').pack(side='left')
+
+        self.qe = tk.Entry(self)
+        self.qe.pack()
+        tk.Label(self, text='angles').pack()
+
+        self.thetae = tk.Entry(self)
+        self.thetae.pack()
+        tk.Label(self, text='theta').pack()
+
+        self.abe = tk.Entry(self)
+        self.abe.pack()
+        tk.Label(self, text='a and b').pack()
+
+        # make them clear content when clicked on 
+        for entry in [self.le, self.qe, self.abe, self.thetae]:
+            entry.bind('<Button-1>', self.clear_entry_text)
+
+        self.subm_butt = tk.Button(self, text='Make Robot')
+        self.subm_butt.pack()
+        self.subm_butt.bind('<Button-1>', self.get_data)
+
+    def get_data(self, event):
+        self.data = []
+        for entry in [self.le, self.qe, self.abe, self.thetae]:
+            text = entry.get()
+            if text != '':
+                try:
+                    self.data.append( 
+                        [float(num) for num in text.strip(' ').split(',')]
+                    )
+                except:
+                    tk.messagebox.showerror('Error', 'entered numbers are not correct, please try again')
+        self.make_robot()
         self.pen.draw()
 
-    def make_robot(self):
-        input = lambda: '100 200; 20 30'
-        dialog = input().split(';')
-        for i, part in enumerate(dialog):
-            dialog[i] = [float(num) for num in part.strip(' ').split(' ')]
+    def clear_entry_text(self, event):
+        event.widget.delete(0, tk.END)
 
+
+    def make_robot(self):
+        dialog = self.data
         if len(dialog) == 2:
             self.robot = Robot(l=dialog[0], q=dialog[1])
         elif len(dialog) == 3:
             self.robot = Robot(l=dialog[0], a=dialog[1][0], b=dialog[2][0])
         elif len(dialog) == 4:
-            self.robot = Robot(l=dialog[0], a=dialog[1][0], b=dialog[2][0], theta=dialog[3])
+            self.robot = Robot(l=dialog[0], a=dialog[1][0], b=dialog[1][1], theta=dialog[2])
+        self.pen.robot = self.robot
 
     def animate_360(self):
         for q0 in range(0, 360):
@@ -41,11 +81,10 @@ class App(tk.Frame):
                 self.robot.q[0][1] = q1
                 self.pen.draw()
 
-
     def make_canvas(self):
-        self.canvas = draw.turtle.Canvas(self, width=800, height=800)
+        self.canvas = tk.Canvas(self, width=400, height=400)
         self.canvas.pack()
 
-        self.pen = draw.Turtle(self.robot, self.canvas)
+        self.pen = draw.Turtle(None, self.canvas)
 
 App().mainloop()
