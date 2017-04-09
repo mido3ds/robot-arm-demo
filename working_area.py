2023a0e736775b6f3d4428b282c72e34a241943e
Mahmoud Adas <mido3ds@gmail.com>
2017-04-09 14:36:22 +0200

    multiprocessed, but cant share state

--- working_area.py
+++ working_area.py
@@ -1,4 +1,5 @@
 import mymath
+import multiprocessing as mpr
 import numpy as np
 from collections import namedtuple
 
@@ -6,17 +7,13 @@ Point = namedtuple('Point', ['x', 'y'])
 
 
 class gl:
-    ''' globals '''
-    def __init__(self):
-        self.pos = 0
-        self.p2, self.p3, self.p4 = Point(0, 0), Point(0, 0), Point(0, 0)
-        self.lq1, self.lq2, self.lq3 = None, None, None
-        self.arr = None
-        self.l = None
-        self.step = None
+    ''' globals namespace '''
+    arr = None
+    l = None
+    step = None
 
 
-def nums_in_range(start, end, step):
+def _nums_in_range(start, end, step):
     n = end - start + 1
     result = n // step
 
@@ -26,55 +23,59 @@ def nums_in_range(start, end, step):
         return result + 1
 
 
-def _calc_xy(q1, q2, q3, myglobal, pos):
-    for i1 in range(q1[0], q1[1] + 1, myglobal.step):
-        for i2 in range(q2[0], q2[1] + 1, myglobal.step):
-            for i3 in range(q3[0], q3[1] + 1, myglobal.step):
-                if i1 != myglobal.lq1:
-                    myglobal.p2 = Point(
-                        myglobal.l[0] * mymath.cosd(i1),
-                        myglobal.l[0] * mymath.sind(i1)
+def _calc_xy(q1, q2, q3, pos):
+    ''' calc part of xy, to be able to make multiprocess '''
+    lq1, lq2 = None, None
+    p2, p3 = Point(0, 0), Point(0, 0)
+
+    print('i have started', 'l is {}'.format(gl.l), 'arr is {}'.format(gl.arr), 'pos is {}'.format(pos))
+    for i1 in range(q1[0], q1[1] + 1, gl.step):
+        for i2 in range(q2[0], q2[1] + 1, gl.step):
+            for i3 in range(q3[0], q3[1] + 1, gl.step):
+                if i1 != lq1:
+                    p2 = Point(
+                        gl.l[0] * mymath.cosd(i1),
+                        gl.l[0] * mymath.sind(i1)
                     )
-                if i2 != myglobal.lq2:
-                    myglobal.p3 = Point(
-                        myglobal.p2.x + myglobal.l[1] * mymath.cosd(i1 + i2),
-                        myglobal.p2.y + myglobal.l[1] * mymath.sind(i1 + i2)
+                if i2 != lq2:
+                    p3 = Point(
+                        p2.x + gl.l[1] * mymath.cosd(i1 + i2),
+                        p2.y + gl.l[1] * mymath.sind(i1 + i2)
                     )
 
                 p4 = Point(
-                    myglobal.p2.x + myglobal.p3.x + myglobal.l[2] * mymath.cosd(i1 + i2 + i3),
-                    myglobal.p2.y + myglobal.p3.y + myglobal.l[2] * mymath.sind(i1 + i2 + i3)
+                    p2.x + p3.x +
+                    gl.l[2] * mymath.cosd(i1 + i2 + i3),
+                    p2.y + p3.y +
+                    gl.l[2] * mymath.sind(i1 + i2 + i3)
                 )
 
                 # store it in array
-                myglobal.arr[pos, 0] = p4.x
-                myglobal.arr[pos, 1] = p4.y
+                gl.arr[pos, 0] = p4.x
+                gl.arr[pos, 1] = p4.y
 
                 pos += 1
-
+    print('i have finished', 'l is {}'.format(gl.l), 'arr is {}'.format(gl.arr), 'pos is {}'.format(pos))
 
 def get_xy(q1, q2, q3, l, step):
     '''return x,y numpy.arrays of points to be plotted
         step: int, step of iteration
     '''
 
-    myglobal = gl()
-
     half_q = int(np.ceil((sum(q1)) / 2))
+    half_size = _nums_in_range(q1[0], half_q, step) \
+        * _nums_in_range(q2[0], q1[1], step) \
+        * _nums_in_range(q3[0], q1[1], step)
 
-    half_size = nums_in_range(q1[0], half_q, step) \
-        * nums_in_range(q2[0], q1[1], step) \
-        * nums_in_range(q3[0], q1[1], step)
+    gl.arr = np.zeros((half_size * 2, 2))
+    gl.l = l
+    gl.step = step
 
-    myglobal.arr = np.zeros((half_size * 2, 2))
-    myglobal.l = l
-    myglobal.step = step
+    mpr.Process(target=_calc_xy, args=((q1[0], half_q), q2, q3, 0)).start()
+    mpr.Process(target=_calc_xy, args=((half_q, q1[1]), q2, q3, half_size)).start()
 
-    _calc_xy((q1[0], half_q), q2, q3, myglobal, pos=0)
-    _calc_xy((half_q, q1[1]), q2, q3, myglobal, pos=half_size)
-
-    x = myglobal.arr[:, 0].view()
-    y = myglobal.arr[:, 1].view()
+    x = gl.arr[:, 0].view()
+    y = gl.arr[:, 1].view()
 
     return x, y
 
